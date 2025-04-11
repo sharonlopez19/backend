bc39ccce7fdfcbc55c143df6feefcfec645ce639
Okami <ozkarrpte@gmail.com>
2025-04-10 21:11:54 -0500

    Actualización para direccionamiento de registro

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:api')->get('/me', [AuthController::class, 'me']);
